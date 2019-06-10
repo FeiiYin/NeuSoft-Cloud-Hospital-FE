c7161dac43e85c25f7124f4b0cdd68c7571b4861
lzw429 <SHUYIHENG2015XIN>
2019-06-10 13:35:03 +0800

    update

--- vue-element/src/api/basicInfo/department.js
+++ vue-element/src/api/basicInfo/department.js
@@ -2,17 +2,35 @@ import request from '@/utils/request'
 
 /**
  * 获取科室列表
- * @param query
+ * @param query 当前页码，页面大小，选择的科室类别等
  * @returns {*}
  */
 export function fetchDepartmentList(query) {
   console.log('fetchDepartmentList: ')
-  // console.log(query)
+  console.log(query)
   return request({
     url: '/department/list',
     method: 'get',
-    params: query, // 当前页码，页面大小，选择的科室类别等
+    params: query,
     baseURL: 'http://localhost:8081/cloud-hospital/',
     contentType: 'application/x-wwww-form-urlencoded'
   })
+}
+
+/**
+ * 添加科室信息
+ * @param query 科室信息
+ */
+export function addDepartment(query) {
+  console.log('addDepartment: ')
+  console.log(query)
+  return request(
+    {
+      url: '/department/add',
+      method: 'post',
+      params: query,
+      baseURL: 'http://localhost:8081/cloud-hospital/',
+      contentType: 'application/x-wwww-form-urlencoded'
+    }
+  )
 }
