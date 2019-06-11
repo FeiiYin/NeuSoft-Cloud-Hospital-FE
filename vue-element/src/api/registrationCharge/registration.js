eafbf94e8744ae301677b6c24d394ca2d7ad63ac
lzw429 <SHUYIHENG2015XIN>
2019-06-11 14:09:41 +0800

    update

--- vue-element/src/api/registrationCharge/registration.js
+++ vue-element/src/api/registrationCharge/registration.js
@@ -17,6 +17,32 @@ export function fetchPatientInfoByIdentifyCardNo(identifyCardNo) {
 }
 
 /**
+ * 获取挂号科室列表
+ */
+export function fetchDepartment() {
+  console.log('fetchDepartment')
+  return request({
+    url: '/registration/department_list',
+    method: 'get',
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
+ * 获取当前可挂号的医生列表
+ */
+export function fetchCurrentAvailableDoctor() {
+  console.log('fetchCurrentAvailableDoctor')
+  return request({
+    url: '/registration/doctor_list',
+    method: 'get',
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
  * 提交注册信息
  * @param registration 注册表单
  */
