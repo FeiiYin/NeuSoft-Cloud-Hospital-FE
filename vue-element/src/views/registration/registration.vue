2fb781068cba0c1155286462d86281c74d8500cc
Samaritan-Infi <1594320575@qq.com>
2019-06-11 11:45:27 +0800

    update

--- vue-element/src/views/registration/registration.vue
+++ vue-element/src/views/registration/registration.vue
@@ -1,119 +1,159 @@
 <template>
-  <div>
-    <div class="wordbox">
+  <div class="app-container">
+    <aside>
       <h2>门诊挂号管理</h2>
+    </aside>
+    <div class="wordbox">      
+      <el-form ref="registrationForm" :model="registrationForm" >
+      <el-row>
+        <el-col :span="17">
+          <el-form-item label="门诊挂号登记号">
+            <el-input v-model="registrationForm.registrationId" :disabled="true"
+              style="width:350px;margin-left:20px;" prefix-icon="el-icon-document">
+            </el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-button type="primary" icon="document" @click="true">
+            <svg-icon icon-class="documentation" />挂号
+          </el-button>
+          <el-button @click="true">
+            预约选择
+          </el-button>
+          <el-button icon="document" @click="true">
+            补收挂号费
+          </el-button>
+        </el-col>
+      </el-row>       
+      <svg-icon style="display:inline-block;margin-right:20px;margin-top:20px;" icon-class="peoples" /> 个人信息
       <hr>
-      <div style="display:inline-block;">
-        <label class="radio-label" style="padding-left:0;">门诊挂号登记号 </label>
-        <el-input v-model="filename" placeholder="100001" style="width:345px;margin-left:20px;" prefix-icon="el-icon-document" />
-      </div>
-      <el-button style="margin:0 0 20px 350px;" type="primary" icon="document" @click="handleDownload">
-        预约选择
-      </el-button>
-      <el-button style="margin:0 0 20px 20px;" type="primary" icon="document" @click="handleDownload">
-        挂号
-      </el-button>
-      <el-button style="margin:0 0 20px 20px;" type="primary" icon="document" @click="handleDownload">
-        补收挂号费
-      </el-button>
-      <div />
-      <svg-icon style="display:inline-block;margin-right:20px;" icon-class="peoples" /> Personal Info
-      <hr>
-      <div class="row">
-        <div class="word_input_4">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <h4 style="margin-bottom:2px;">身份证号</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.identityCardNo"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <h4 style="margin-bottom:2px;">家庭住址</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.familyAddress"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="6">
           <h4 style="margin-bottom:2px;">姓名</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
+          <el-form-item >
+            <el-input v-model="registrationForm.patientName"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
           <h4 style="margin-bottom:2px;">性别</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
+          <el-form-item >
+            <el-select v-model="registrationForm.gender" placeholder="请选择性别"
+              style="width:100%">
+              <el-option label="男" value="男"></el-option>
+              <el-option label="女" value="女"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
           <h4 style="margin-bottom:2px;">年龄</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
+          <el-form-item >
+            <el-input v-model="registrationForm.age"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
           <h4 style="margin-bottom:2px;">出生日期</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="word_input_2">
-          <h4 class="row_h4">身份证号</h4>
-          <input id="projectinput1" type="text" class="registration_input_2" placeholder="" name="fname">
-        </div>
-        <div class="word_input_2">
-          <h4 class="row_h4">家庭住址</h4>
-          <input id="projectinput1" type="text" class="registration_input_2" placeholder="" name="fname">
-        </div>
-      </div>
-
-      <svg-icon style="display:inline-block;margin-right:20px;margin-top:20px;" icon-class="tab" /> Required Info
-      <hr>
-
-      <div class="row">
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">病历号</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">结算类别</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">医疗证号</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
+          <el-form-item >
+            <el-date-picker type="date" placeholder="选择日期" v-model="registrationForm.birthday" 
+              style="width: 100%;">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <svg-icon style="margin-right:20px;margin-top:40px;" icon-class="tab" /> 病历信息
+      <hr>  
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">挂号类型</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.registrationCategory"></el-input>
+          </el-form-item>          
+        </el-col>
+        <el-col :span="6">
+          <!-- 普通或者城镇职工 -->
           <h4 style="margin-bottom:2px;">医疗类别</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">看诊日期</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">号列</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">挂号科室</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">看诊医生</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="word_input_4">
+          <el-form-item >
+            <el-input v-model="registrationForm.medicalCategory"></el-input>
+          </el-form-item>          
+        </el-col>
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">医疗证号</h4>
+          <el-input ></el-input>
+        </el-col>
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">医疗类别</h4>
+          <el-input ></el-input>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">挂号日期</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.registrationDate"></el-input>
+          </el-form-item>  
+        </el-col>
+        <el-col :span="6">
           <h4 style="margin-bottom:2px;">挂号来源</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
-          <h4 style="margin-bottom:2px;">医保诊断</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-        <div class="word_input_4">
+          <el-form-item >
+            <el-input v-model="registrationForm.registrationSource"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">挂号科室</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.departmentId"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">看诊医生</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.doctorId"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">当前收费员Id</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.collectorId"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <h4 style="margin-bottom:2px;">结算类别</h4>
+          <el-form-item >
+            <el-input v-model="registrationForm.settleAccountsCategory"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
           <h4 style="margin-bottom:2px;">病历本</h4>
-          <el-radio v-model="radio" label="1">是</el-radio>
-          <el-radio v-model="radio" label="2">否</el-radio>
-        </div>
-        <div class="word_input_4">
+          <el-radio v-model="medicalRecordRadio" label="1">是</el-radio>
+          <el-radio v-model="medicalRecordRadio" label="2">否</el-radio>
+        </el-col>
+        <el-col :span="6">
           <h4 style="margin-bottom:2px;">应收金额</h4>
-          <input id="projectinput1" type="text" class="registration_input" placeholder="" name="fname">
-        </div>
-      </div>
-
+          <el-form-item >
+            <el-input v-model="registrationForm.totalCharge"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
       <div style="margin-top:80px;margin-bottom:40px;text-align:center">
         <el-button type="primary">提交</el-button>
         <el-button type="info">清空</el-button>
       </div>
+      </el-form>
     </div>
   </div>
 
@@ -123,7 +163,32 @@
 export default {
   data() {
     return {
-      radio: '1'
+      // 病历本是否需要
+      medicalRecordRadio: '1',
+      // 表单
+      registrationForm: {
+        registrationId: '',
+        patientName: '',
+        patientId: '',
+        gender: '',
+        age: '',
+        birthday: '',
+        registrationCategory: '',
+        medicalCategory: '',
+        identityCardNo: '',
+        registrationStatus: '',
+        visitDate: '',
+        registrationDate: '',
+        departmentId: '',
+        doctorId: '',
+        registrationSource: '',
+        settleAccountsCategory: '',
+        isVisited: '',
+        valid: '',
+        familyAddress: '',
+        collectorId: '',
+        totalCharge: '',
+      }
     }
   }
 }
@@ -131,7 +196,6 @@ export default {
 
 <style  scoped>
   .wordbox {
-    margin: 2%;
     display: inline-block;
     background: #eef1f6;
     padding: 8px 24px;
@@ -144,51 +208,5 @@ export default {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
     color: #2c3e50;
     -webkit-font-smoothing: antialiased;
-  }
-  .row {
-    display:inline-block;
-    margin: 1%;
-    width: 100%;
-  }
-  .word_input_2 {
-    width: 49%;
-    height: 46px;
-    display: inline-block;
-  }
-  .word_input_4 {
-    width: 24.5%;
-    height: 46px;
-    display: inline-block;
-  }
-  .registration_input {
-    position: relative;
-    padding: 4px 7px;
-    width: 80%;
-    height: 30px;
-    cursor: text;
-    font-size: 12px;
-    line-height: 1.5;
-    color: rgba(0, 0, 0, .65);
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #d9d9d9;
-    border-radius: 4px;
-  }
-  .registration_input_2 {
-    position: relative;
-    padding: 4px 7px;
-    width: 91%;
-    height: 30px;
-    cursor: text;
-    font-size: 12px;
-    line-height: 1.5;
-    color: rgba(0, 0, 0, .65);
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #d9d9d9;
-    border-radius: 4px;
-  }
-  .row_h4 {
-    margin-bottom:2px;
   }
 </style>
