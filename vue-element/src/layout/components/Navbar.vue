9709d823467889495b95475eb3b7159cfa474e6d
lzw429 <SHUYIHENG2015XIN>
2019-06-11 09:55:55 +0800

    update

--- vue-element/src/layout/components/Navbar.vue
+++ vue-element/src/layout/components/Navbar.vue
@@ -12,7 +12,7 @@
 
         <screenfull id="screenfull" class="right-menu-item hover-effect" />
 
-        <el-tooltip content="Global Size" effect="dark" placement="bottom">
+        <el-tooltip content="全局大小" effect="dark" placement="bottom">
           <size-select id="size-select" class="right-menu-item hover-effect" />
         </el-tooltip>
 
