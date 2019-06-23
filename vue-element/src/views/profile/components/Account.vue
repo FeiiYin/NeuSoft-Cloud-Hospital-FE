3c3ca8d99834de029cd12da09dd54340ffc2bdcf
lzw429 <SHUYIHENG2015XIN>
2019-06-23 11:20:23 +0800

    update

--- vue-element/src/views/profile/components/Account.vue
+++ vue-element/src/views/profile/components/Account.vue
@@ -1,29 +1,39 @@
 <template>
   <el-form>
-    <el-form-item label="Name">
-      <el-input v-model.trim="user.name" />
+    <el-form-item label="姓名">
+      <el-input v-model.trim="user.name" :disabled="true" />
     </el-form-item>
-    <el-form-item label="Email">
-      <el-input v-model.trim="user.email" />
+    <el-form-item label="身份">
+      <el-input v-model.trim="user.jobTitle" :disabled="true" />
     </el-form-item>
+    <el-form-item label="科室">
+      <el-input v-model.trim="user.departmentName" :disabled="true" />
+    </el-form-item>
+
+    <el-form-item label="参与排班">
+      <el-switch
+        v-model.trim="user.doctorScheduling"
+        style="display: block"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        active-text="是"
+        inactive-text="否"
+      />
+    </el-form-item>
+
     <el-form-item>
-      <el-button type="primary" @click="submit">Update</el-button>
+      <el-button type="primary" @click="submit">提交</el-button>
     </el-form-item>
   </el-form>
 </template>
 
 <script>
 export default {
-  props: {
-    user: {
-      type: Object,
-      default: () => {
-        return {
-          name: '',
-          email: ''
-        }
-      }
-    }
+  user: {
+    name: '',
+    jobTitle: '',
+    departmentName: '',
+    doctorScheduling: true
   },
   methods: {
     submit() {
---
+++ vue-element/src/views/profile/components/AccountSecurity.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-form>
+    <el-form-item>
+      <el-input v-model="user.newPassword" placeholder="新密码" show-password />
+    </el-form-item>
+  </el-form>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value1: true,
+        value2: true
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
