fc36b86aa068510d1857e9c6145272f39018be97
Frank <shuhaosmile@gmail.com>
2019-06-23 19:05:23 +0800

    modify account（密码不现实，修复 本页条目 bug）

--- vue-element/src/api/basicInfo/schedule.js
+++ vue-element/src/api/basicInfo/schedule.js
@@ -1,9 +1,127 @@
 import request from '@/utils/request'
-
+/**
+  * @Description: 获取排班规则
+  * @Param:
+  * @Return:
+  * Author: Shuhao Xu
+  * Date:
+  * Time:
+  */
 export function fetchList(query) {
+  console.log('传到服务器的参数', query)
   return request({
-    url: '/doctor_scheduling/list',
+    url: '/scheduling/select_rule',
     method: 'get',
-    params: query
+    params: query,
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
+  * @Description: 保留排班规则: 新增及修改
+  * @Param:
+  * @Return:
+  * Author: Shuhao Xu
+  * Date:
+  * Time:
+  */
+export function saveSchedulingRule(param) {
+  return request({
+    url: '/scheduling/save_rule',
+    method: 'get',
+    params: param,
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
+  * @Description: 删除排班规则
+  * @Param:
+  * @Return:
+  * Author: Shuhao Xu
+  * Date:
+  * Time:
+  */
+export function deleteSchedulingRule(param) {
+  return request({
+    url: '/scheduling/delete_rule',
+    method: 'delete',
+    params: param,
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
+  * @Description: 生成排班计划
+  * @Param:
+  * @Return:
+  * Author: Shuhao Xu
+  * Date:
+  * Time:
+  */
+export function generateSchedulingInfo(param) {
+  return request({
+    url: '/scheduling/generate_info',
+    method: 'get',
+    params: param,
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
+  * @Description: 获取排班信息
+  * @Param:
+  * @Return:
+  * Author: Shuhao Xu
+  * Date:
+  * Time:
+  */
+export function selectSchedulingInfo(param) {
+  return request({
+    url: '/scheduling/select_info',
+    method: 'get',
+    params: param,
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
+  * @Description: 保留排班信息，update
+  * @Param:
+  * @Return:
+  * Author: Shuhao Xu
+  * Date:
+  * Time:
+  */
+export function saveSchedulingInfo(param) {
+  return request({
+    url: '/scheduling/update_info',
+    method: 'put',
+    params: param,
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
+  })
+}
+
+/**
+  * @Description: 删除排班信息
+  * @Param:
+  * @Return:
+  * Author: Shuhao Xu
+  * Date:
+  * Time:
+  */
+export function deleteSchedulingInfo(param) {
+  return request({
+    url: '/scheduling/delete_info',
+    method: 'delete',
+    params: param,
+    baseURL: 'http://localhost:8081/cloud-hospital/',
+    contentType: 'application/x-wwww-form-urlencoded'
   })
 }
--- vue-element/src/views/basicInfo/schedule.vue
+++ vue-element/src/views/basicInfo/schedule.vue
@@ -1,12 +1,12 @@
 <template>
   <div style="padding:1%;" class="app-container">
     <aside>医生排班规则维护</aside>
-
+<!-- todo 做排班表-->
     <el-table
       ref="multipleTable"
       v-loading="listLoading"
       highlight-current-row
-      :data="diseaseTable"
+      :data="schedulingTable"
       tooltip-effect="dark"
       style="width: 100%"
       @selection-change="handleSelectionChange"
@@ -14,8 +14,8 @@
 
       <el-table-column type="selection" width="55" />
 
-      <el-table-column prop="diseaseCode" label="疾病编码" width="120">
-        <template slot-scope="scope">{{ scope.row.diseaseCode }}</template>
+      <el-table-column prop="schedulingCode" label="排班编码" width="120">
+        <template slot-scope="scope">{{ scope.row.schedulingCode }}</template>
       </el-table-column>
     </el-table>
     <aside>
@@ -43,9 +43,9 @@
 
     <el-table
       ref="multipleTable"
-      v-loading="listLoading"
+      v-loading="listLoading2"
       highlight-current-row
-      :data="diseaseTable"
+      :data="schedulingTable"
       tooltip-effect="dark"
       style="width: 100%"
       @selection-change="handleSelectionChange"
@@ -53,19 +53,21 @@
 
       <el-table-column type="selection" width="55" />
 
-      <el-table-column prop="diseaseCode" label="疾病编码" width="120">
-        <template slot-scope="scope">{{ scope.row.diseaseCode }}</template>
+      <el-table-column prop="schedulingCode" label="疾病编码" width="120">
+        <template slot-scope="scope">{{ scope.row.schedulingCode }}</template>
       </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
-// import { fetchList } from '@/api/basicInfo/schedule.js'
+import { fetchList, saveSchedulingRule, deleteSchedulingRule, generateSchedulingInfo, selectSchedulingInfo, saveSchedulingInfo, deleteSchedulingInfo} from '../../api/basicInfo/schedule.js'
 
 export default {
   data() {
     return {
+      listLoading: false, // 加载表格
+      listLoading2: false, // 医生排班信息表
       form: {
         name: '',
         region: '',
@@ -75,6 +77,52 @@ export default {
         type: [],
         resource: '',
         desc: ''
+      },
+      schedulingTable: {},
+      currentPage: 1,
+      pageSize: 20,
+      // js 方法的组成  的参数
+      listQuery: {
+        currentPage: -1,
+        pageSize: -1
+      },
+      saveSchedulingRuleParam: {
+        schedulingRuleId: -1, // 排班计划编号；新增时不填，修改时必填
+        weekday: -1, // 排班星期：星期日 0，星期六 6
+        departmentId: -1, // 排班医生所在科室的编号
+        doctorId: -1, // 排班医生的编号
+        registrationCategoryId: -1, // 挂号类别，详见 registration_category 表
+        noon: -1, // 午别：全天 0；上午 1；下午 2
+        limitation: -1, // 排班限额
+        accountId: -1 // 操作员的帐号
+      },
+      deleteSchedulingRuleParam: {
+        schedulingRuleIdList: []
+      },
+      generateSchedulingInfoParam: {
+        startDate: '1999-12-31',
+        endDate: '2000-01-01'
+      },
+      selectSchedulingInfoParam: {
+        currentPage: -1,
+        pageSize: -1
+      },
+      saveSchedulingInfoParam: {
+        schedulingInfoId: -1,
+        schedulingTime: '',
+        departmentId: -1,
+        doctorId: -1,
+        registrationCategoryId: -1,
+        valid: -1,
+        noon: -1,
+        limitation: -1,
+        remainNums: -1
+      },
+      deleteSchedulingInfoParam: {
+        schedulingInfoIdList: []
+      },
+      rules: {
+
       }
     }
   },
@@ -84,13 +132,44 @@ export default {
   },
 
   methods: {
-    // getList() { // 获取列表
-    //   this.listLoading = true // 列表正在加载
-    //   fetchList(this.listQuery).then(response => {
-    //     // todo
-    //     this.listLoading = false // 列表加载完成
-    //   })
-    // }
+    // 更新表格的 变量 匹配 方法
+    // 删除表格的 选择框方法
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+      console.log('handle de 值是', val)
+    },
+    // 和 js 相匹配的方法
+    getList() { // 获取列表
+      this.listLoading = true // 列表正在加载
+      this.listQuery.currentPage = this.currentPage
+      this.listQuery.pageSize = this.pageSize
+      console.log('查询传过去的参数', this.listQuery)
+      fetchList(this.listQuery).then(response => {
+        this.schedulingTable = response.data.list
+        console.log('查询成功后的response', response)
+        this.listLoading = false // 列表加载完成
+      }).catch(error => {
+        console.log('在返回之后出现的问题', error)
+      })
+    },
+    saveSchedulingRuleV() {
+
+    },
+    deleteSchedulingRuleV() {
+
+    },
+    generateSchedulingInfoV() {
+
+    },
+    selectSchedulingInfoV() {
+
+    },
+    saveSchedulingInfoV() {
+
+    },
+    deleteSchedulingInfoV() {
+
+    }
   }
 }
 </script>
